39d2af31a9a3e803656830394993790daf216175
Lauren Achtem <achtemlauren@gmail.com>
2015-04-17 22:50:16 -0700

    First commit

---
+++ recipe_script.js
@@ -0,0 +1,51 @@
+
+$(document).ready(function() {
+  console.log("Script included!");
+  $('.liPasta').click(function() {
+  $('.liPasta').fadeTo("slow",0.5);
+});
+  $('.liOlive').click(function() {
+  $('.liOlive').fadeTo("slow",0.5);
+});
+  $('.liSalt').click(function() {
+  $('.liSalt').fadeTo("slow",0.5);
+});
+  $('.liYolk').click(function() {
+  $('.liYolk').fadeTo("slow",0.5);
+});
+  $('.liGrated').click(function() {
+  $('.liGrated').fadeTo("slow",0.5);
+});
+  $('.liFresh').click(function() {
+  $('.liFresh').fadeTo("slow",0.5);
+});
+  $('.liMilk').click(function() {
+  $('.liMilk').fadeTo("slow",0.5);
+});
+  $('.liCourse').click(function() {
+  $('.liCourse').fadeTo("slow",0.5);
+});
+  $('.liSlab').click(function() {
+  $('.liSlab').fadeTo("slow",0.5);
+});
+ $('button').click(function() {
+     $('img').fadeOut("slow");   
+});
+
+
+$('.caption1').click(function() {
+  $('.caption1').fadeTo("slow",0.5);
+});
+$('.caption2').click(function() {
+  $('.caption2').fadeTo("slow",0.5);
+});
+$('.caption3').click(function() {
+  $('.caption3').fadeTo("slow",0.5);
+});
+$('.caption4').click(function() {
+  $('.caption4').fadeTo("slow",0.5);
+});
+$('.caption5').click(function() {
+  $('.caption5').fadeTo("slow",0.5);
+});
+});
