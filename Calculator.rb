f95c71bbc6a6ef79be2384f64973c24e14a08563
Lauren Achtem <achtemlauren@gmail.com>
2015-04-19 13:09:57 -0700

    First commit

---
+++ Calculator.rb
@@ -0,0 +1,12 @@
+Calculator.rb
+class Calculator
+  def add(a, b)
+    return (a + b)
+    puts ("The sum is #{return}")
+  end
+
+  def subtract(a, b)
+   return (a - b)
+    puts ("The sum is #{return}")
+  end
+end
