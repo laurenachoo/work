3995a23f8dac383598866a9fc2e087037088e57a
Lauren Achtem <laurenachoo@users.noreply.github.com>
2015-04-23 23:13:38 -0700

    Update Calculator.rb
    I simplified this time by taking out the 'return', 'puts' and the string, and just ran the code to get the answers on RubyMonk

--- Calculator.rb
+++ Calculator.rb
@@ -1,12 +1,10 @@
-Calculator.rb
+
 class Calculator
   def add(a, b)
-    return (a + b)
-    puts ("The sum is #{return}")
-  end
+    a + b
+   end
 
   def subtract(a, b)
-   return (a - b)
-    puts ("The sum is #{return}")
-  end
+    a - b
+    end
 end
